c0431e240ccdd090d8b78c447f7ec88b73fcc74a
mashhur-dev <mashhurtadjiyev@gmail.com>
2022-11-29 21:35:02 +0500

    11-dars

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,62 @@
-<script>
-	export let name;
+<script lang="ts">
+	import type { text } from "svelte/internal";
+
+	let pupils: string[] = [
+		"Arslon",
+		"Otabek",
+		"Jahongir",
+		"Firdavs",
+		"Sevinchbek",
+		"Mashhurbek",
+		"Islam",
+		"Hasanboy",
+		"Komron",
+		"Joshqin",
+	];
+
+	type Post = {
+		userId: number;
+		id: number;
+		title: string;
+		body: string;
+	};
+
+	let posts: Post[] = [];
+
+	let promise: Promise<Response> = fetch(
+		"https://jsonplaceholder.typicode.com/posts"
+	);
+
+	promise
+		.then((mashhurbek) => mashhurbek.text())
+		.then((text) => {
+			const array = JSON.parse(text);
+			posts = array;
+		});
+	function remove(id: number) {
+		posts = posts.filter((post) => post.id != id);
+	}
+
+	function removes() {
+		posts = posts.filter((item) => typeof item.id == "string");
+	}
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<h1 class="ms-1">Post</h1>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+<div class="d-flex gap-2 flex-wrap">
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+	<button on:click={() => removes()} class="btn btn-danger">Delete All</button>
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+	{#each posts as item}
+		<div class="card m-3" style="width: 250px;">
+			<div class="card-body">
+				<h3>{item.id}: {item.title}</h3>
+				<p>{item.body}</p>
+				<button on:click={() => remove(item.id)} class="btn btn-primary">
+					Delete
+				</button>
+			</div>
+		</div>
+		{/each}	
+</div>
